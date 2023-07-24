6c8c5500dfa9dd1d1ca8f16a679de6114af0c721
Emīls Valdmanis <valdmanis71@gmail.com>
2023-07-24 16:33:51 +0300

    Functionality for top/bottom text

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -5,9 +5,9 @@ export default function Meme() {
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
-    randomImage: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
   });
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+  const [allMemeImages] = React.useState(memesData);
 
   function handleOnClick() {
     setMeme((prevMeme) => ({
@@ -17,21 +17,43 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="meme--form">
         <div className="meme--inputs">
-          <input type="text" className="meme--input" placeholder="Top Text" />
           <input
             type="text"
+            name="topText"
+            className="meme--input"
+            placeholder="Top Text"
+            onChange={handleChange}
+            value={meme.topText}
+          />
+          <input
+            type="text"
+            name="bottomText"
             className="meme--input"
             placeholder="Bottom Text"
+            onChange={handleChange}
+            value={meme.bottomText}
           />
         </div>
         <button className="meme--button" onClick={handleOnClick}>
           Get new meme image 🖼️
         </button>
-        <img src={meme.randomImage} className="meme--image" />
+        <div className="meme">
+          <img src={meme.randomImage} className="meme--image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
       </div>
     </main>
   );
